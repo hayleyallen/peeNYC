023a567d118cf87d5d6b9c23ae9642a0e1c13cc8
mam10023 <144063529+mam10023@users.noreply.github.com>
2023-12-03 19:38:26 +0000

    delete other try/catch

--- main.java
+++ main.java
@@ -6,36 +6,7 @@ import java.util.*;
 
 public class main {
 	public static void main(String[] args) throws IOException{
-    	 Scanner input = new Scanner(System.in);
-    	 
-         String name;
-         String stars;
-         String address;
-         String latitude;
-         String longitude;
-         String code;
-         String accessibility;
-
-    	 try {
-    		 FileReader fileReader = new FileReader("publicBathrooms");
-    		 BufferedReader bufferedReader = new BufferedReader(fileReader);
-
-             while((line = bufferedReader.readLine())!=null){
-                String [] entries = line.split(",");
-
-                name = entries[0].trim();
-                stars = entries[1].trim();
-                address = ;
-                latitude;
-                longitude;
-                code;
-                accessibility;
-             }
-    	 } catch(IOException E) {
-    		 
-    	 }
-
-
+    	
 		String bathroomFile = "publicBathrooms";
 		String line = null;
 
