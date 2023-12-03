12d7778c38bb6af8769118f31935a1e55fcaa745
mam10023 <144063529+mam10023@users.noreply.github.com>
2023-12-03 18:53:57 +0000

    changes to distanceCalc

--- DistanceCalculator.java
+++ DistanceCalculator.java
@@ -1,6 +1,17 @@
 public class DistanceCalculator {
 
     public static double distanceCalculator(long userLat, long userLong, BathroomEntry bathroom) {
+        long bathroomLat = bathroom.getLatitude();
+        long bathroomLong = bathroom.getLongitude();
+
+        /*
+         * 2R × sin⁻¹(√[sin²((θ₂ - θ₁)/2) + cosθ₁ × cosθ₂ × sin²((φ₂ - φ₁)/2)])
+         * θ₁, φ₁ – First point latitude and longitude coordinates;
+        θ₂, φ₂ – Second point latitude and longitude coordinates;
+        R – Earth's radius (R = 6371 km); and
+         */
+
+         double distance = 2(6371) * sin
 
     }
 }
