122195cba5e8f8d08b25a2ab5a9eaefde9a4a5db
mam10023 <144063529+mam10023@users.noreply.github.com>
2023-12-03 19:02:05 +0000

    created methods to calculate distance between two coordinates

--- DistanceCalculator.java
+++ DistanceCalculator.java
@@ -1,17 +1,34 @@
 public class DistanceCalculator {
 
+    // Radius of the Earth in kilometers
+    private static final long EARTH_RADIUS_KM = 6371L;
+
+    // Convert degrees to radians
+    private static double toRadians(long degrees) {
+        return Math.toRadians(degrees / 1e6);
+    }
+
+    // Haversine formula to calculate distance between two points
+    public static double haversineDistance(long lat1, long lon1, long lat2, long lon2) {
+        double dLat = toRadians(lat2 - lat1);
+        double dLon = toRadians(lon2 - lon1);
+
+        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+                Math.cos(toRadians(lat1)) * Math.cos(toRadians(lat2)) *
+                        Math.sin(dLon / 2) * Math.sin(dLon / 2);
+
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+        return EARTH_RADIUS_KM * c;
+    }
+
     public static double distanceCalculator(long userLat, long userLong, BathroomEntry bathroom) {
         long bathroomLat = bathroom.getLatitude();
         long bathroomLong = bathroom.getLongitude();
 
-        /*
-         * 2R × sin⁻¹(√[sin²((θ₂ - θ₁)/2) + cosθ₁ × cosθ₂ × sin²((φ₂ - φ₁)/2)])
-         * θ₁, φ₁ – First point latitude and longitude coordinates;
-        θ₂, φ₂ – Second point latitude and longitude coordinates;
-        R – Earth's radius (R = 6371 km); and
-         */
+        double distance = haversineDistance(userLat, userLong, bathroomLat, bathroomLong);
 
-         double distance = 2(6371) * sin
-
+        return distance;
     }
+
 }
