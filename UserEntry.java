42d0cec69bc9bb24d08ce1e323c6fb9d6c2cc92b
mam10023 <144063529+mam10023@users.noreply.github.com>
2023-12-03 18:34:20 +0000

    fix errors - constructor name

--- UserEntry.java
+++ UserEntry.java
@@ -1,47 +1,47 @@
 import java.util.Scanner;
 
 public class UserEntry {
-	//data fields
+	// data fields
 	private String username;
 	private String password;
-    private String name;
-	
-	//constructor for username and password
-	public User(String user, String pass, String name) {
+	private String name;
+
+	// constructor for username and password
+	public UserEntry(String user, String pass, String name) {
 		this.username = user;
 		this.password = pass;
-        this.name = name;
+		this.name = name;
 	}
-	
-	//getters and setters
+
+	// getters and setters
 	public String getUsername() {
 		return username;
 	}
-	
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
-	
+
 	public String getPassword() {
 		return password;
 	}
-	
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
 
-    public String getName() {
+	public String getName() {
 		return name;
 	}
-	
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
-	//print username, password, name
+
+	// print username, password, name
 	public void PrintUserInfo() {
-		System.out.println("Username: "  + username);
-        System.out.println("Name: " + name);
+		System.out.println("Username: " + username);
+		System.out.println("Name: " + name);
 		System.out.println("Password: " + password);
 	}
 }
