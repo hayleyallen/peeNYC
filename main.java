4ece2d142cf99611c252901393583c528898815d
haleyhobbs <106222701+haleyhobbs@users.noreply.github.com>
2023-12-03 15:00:11 -0500

    sdv

--- main.java
+++ main.java
@@ -1,50 +1,57 @@
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
 import java.util.*;
 
-public class main {
-	public static void main(String[] args) throws IOException {
+public class main{
+    public static void main(String[] args) throws IOException{
+    	 Scanner input = new Scanner(System.in);
+    	 
+    	 try {
+    		 FileReader fileReader = new FileReader("publicBathrooms");
+    		 
+    		 
+    	 } catch(IOException E) {
+    		 
+    	 }
+    	 
 
-		String bathroomFile = "publicBathrooms";
-		String line = null;
+         ArrayList <BathroomEntry> publicBathrooms = new FileReader(pubb)
+    }
 
-		String bathroomName = "";
-		String bathroomRating = "";
-		String bathroomAddress = "";
-		String bathroomLat = "";
-		String bathroomLong = "";
-		String bathroomCode = "";
-		String bathroomAccesibility = "";
+	//check if user has an account
+	public void login() {
+		int x = 1;
+		System.out.println("Do you have an account? y/n: ");
+		if (input.next() == "y") {
+			while (x == 1) {
+				System.out.print("Enter username: ");
+				String username = input.next();
+				System.out.print("Enter password: ");
+				String password = input.next();
 
-		try {
-			FileReader fileReader = new FileReader("publicBathrooms.txt");
-
-			BufferedReader bufferedReader = new BufferedReader(fileReader);
-
-			while ((line = bufferedReader.readLine()) != null) {
-				ArrayList<String> entries = new ArrayList<>();
-
-				String[] tmp = line.split(",");
-
-				entries.add(tmp[0].trim());
-				entries.add(tmp[1].trim());
-				entries.add(tmp[2].trim());
-				entries.add(tmp[3].trim());
-				entries.add(tmp[4].trim());
-				entries.add(tmp[5].trim());
-				entries.add(tmp[6].trim());
-
+				int index = UserDirectory().accountCheck(username, password);
+				if (index == -2) {
+					System.out.println("We couldn't find an account with that information. Enter 1 to try again or 0 to exit.");
+					if (input.nextInt() == 0)
+						x = 0;
+				} else {
+					System.out.println("Hi " + (UserDirectory().findName(i)));
+					x = 0;
+				}
 			}
-
-			bufferedReader.close();
-		} catch (FileNotFoundException ex) {
-			System.out.println("Unable to open file '" + bathroomFile + "'");
-		} catch (IOException ex) {
-			System.out.println("Error reading file '" + bathroomFile + "'");
 		}
+	}
 
-		// ArrayList <BathroomEntry> publicBathrooms = //
+	//create account
+	public void createAccount() {
+		System.out.println("Name: ");
+		String name = input.nextLine();
+		System.out.println("Username: ");
+		String username = input.nextLine();
+		System.out.println("Password: ");
+		String password = input.nextLine();
+		User newUser = new User(name, username, password);
+		UserDirectory().addEntry(newUser);
 	}
 }
