95b0e74ba0e64fc87f01919613210bcacca4a890
mam10023 <144063529+mam10023@users.noreply.github.com>
2023-12-03 20:47:47 +0000

    fix errors in variable names

--- BathroomDirectory.java
+++ BathroomDirectory.java
@@ -3,23 +3,32 @@ import java.util.ArrayList;
 public class BathroomDirectory {
     private ArrayList<BathroomEntry> bathroomList = new ArrayList<BathroomEntry>();
 
-    //add entry
-	public void addEntry(BathroomEntry entry) {
-		bathroomList.add(entry)
-	}
+    // add entry
+    public void addEntry(BathroomEntry entry) {
+        bathroomList.add(entry);
+    }
 
-    //search for bathroom by address and delete entry (if bathroom no longer there)
-	public void DeleteEntry(String address) {
-	    for (int i = 0; i < ArrayList.size(); i++) {
-	        if (entry.get(i).getAddress() == address) {
-	            entry.remove(i);
-	        }
-	    }
-	}
+    // search for bathroom by address and delete entry (if bathroom no longer there)
+    public void DeleteEntry(String address) {
+        for (int i = 0; i < ArrayList.size(); i++) {
+            if (entry.get(i).getAddress() == address) {
+                entry.remove(i);
+            }
+        }
+    }
 
-	//print all bathroom names
-	public void printBathrooms() {
-		for (BathroomEntry bathroom : bathroomList)
-			System.out.println(bathroom.getName());
-	}
+    // print all bathroom names
+    public void printBathrooms() {
+        for (BathroomEntry bathroom : bathroomList)
+            System.out.println(bathroom.getName());
+    }
+
+    // search for bathroom by address and delete entry (if bathroom no longer there)
+    public void DeleteEntry(String address) {
+        for (int i = 0; i < bathroomList.size(); i++) {
+            if (bathroomList.get(i).getAddress() == address) {
+                bathroomList.remove(i);
+            }
+        }
+    }
 }
