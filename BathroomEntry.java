b2fe3a743b622b80166ff6e22b010482322b239f
haleyhobbs <106222701+haleyhobbs@users.noreply.github.com>
2023-12-03 12:13:15 -0500

    bathroom entry edits

--- BathroomEntry.java
+++ BathroomEntry.java
@@ -12,41 +12,70 @@ public class BathroomEntry {
 	public BathroomEntry() {
 	}
 		
-	public BathroomEntry(String entryName, double entryRating, String entryNotes, String entryAddress) {
+    //constructor: all fields
+	public BathroomEntry(String entryName, double entryRating, String entryNotes, String entryAddress, boolean entryCodeStatus) {
 		name = entryName;
 		rating = entryRating;
 		notes = entryNotes;
 		address = entryAddress;
+        codeStatus = entryCodeStatus;
 	}
 	
+    //constructor: name, address
 	public BathroomEntry(String entryName, String entryAddress) {
 		name = entryName;
 		address = entryAddress;
 	}
-	
+	//constructor: name, address, rating
 	public BathroomEntry(String entryName, double entryRating, String entryAddress) {
 		name = entryName;
 		rating = entryRating;
 		address = entryAddress;
 	}
-	
+
+	//constructor: name, address, notes
 	public BathroomEntry(String entryName, String entryNotes, String entryAddress) {
 		name = entryName;
 		notes = entryNotes;
 		address = entryAddress;
 	}
-	
+	//constructor: name, address, code status
 	public BathroomEntry(String entryName, boolean entryCodeStatus, String entryAddress) {
 		name = entryName;
 		codeStatus = entryCodeStatus;
 		address = entryAddress;
 	}
-		
+
+    //constructor: name, address, rating, notes
+    public BathroomEntry(String entryName, double entryRating, String entryNotes, String entryAddress) {
+		name = entryName;
+		rating = entryRating;
+        notes = entryNotes;
+		address = entryAddress;
+	}
+
+    //constructor: name, address, rating, code status
+    public BathroomEntry(String entryName, double entryRating, boolean entryCodeStatus, String entryAddress) {
+		name = entryName;
+		rating = entryRating;
+        codeStatus = entryCodeStatus;
+		address = entryAddress;
+	}
+
+    //constructor: name, address, notes, code status
+	public BathroomEntry(String entryName, String entryNotes, boolean entryCodeStatus, String entryAddress) {
+		name = entryName;
+		codeStatus = entryCodeStatus;
+        notes = entryNotes;
+		address = entryAddress;
+	}
+
 	//display info
 	public void printBathroomEntry() {
 		System.out.println("Name: " + getName());
 		System.out.println("Location: " + getAddress());
 		System.out.println("Rating: " + getRating());
+        System.out.println("Code status: " + getCodeStatus());
 		System.out.println("Notes: " + getNotes());
 	}
 
@@ -81,6 +110,14 @@ public class BathroomEntry {
 
 	public void setAddress(String address) {
 		this.address = address;
+	}
+
+    public boolean getCodeStatus() {
+		return address;
+	}
+
+	public void setCodeStatus(boolean codeStatus) {
+		this.codeStatus = codeStatus;
 	}
 
 }
